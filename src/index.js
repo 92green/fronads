8dab77de73359a3de82ff5ec3d4b38e8976ad15a
Allan Hortle <allanhortle@gmail.com>
2017-05-01 16:01:41 +1000

    fix exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 //@flow
 export {
+    default as RequestState,
     RequestEmpty,
     RequestError,
     RequestFetching,
-    RequestState,
     RequestSuccess
 } from './RequestState';
+
+export {
+    default as Maybe,
+    Some,
+    None
+} from './Maybe';
+
+export {
+    default as Either,
+    Left,
+    Right,
+    Try
+} from './Either';
